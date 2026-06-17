d189757808c3c035fa91cf8717a136b30381b685
Monalisa Alves <Monalisaoliveiraalves123@gmail.com>
2026-06-17 14:38:37 -0300

    ve se funciona

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -21,6 +21,9 @@ const sendVerificationEmail = async (email, token) => {
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
