81d51afcf00625a33d7d8acd80867ddd3ee5d1cc
Monalisa Alves <Monalisaoliveiraalves123@gmail.com>
2026-06-17 13:26:57 -0300

    tentativa1090

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -17,13 +17,18 @@ const sendVerificationEmail = async (email, token) => {
     console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? 'EXISTE' : 'UNDEFINED');
     console.log('API_URL:', process.env.API_URL);
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     console.log('Verificando conexão SMTP...');
 
